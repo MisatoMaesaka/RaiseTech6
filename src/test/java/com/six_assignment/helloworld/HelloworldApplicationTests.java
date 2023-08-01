ffd429d87f8b82d05ac33b24e85a982660a21fc2
Misato-Maesaka <r012mbmaesaka@gmail.com>
2023-08-01 11:01:34 +0900

    first commit

---
+++ src/test/java/com/six_assignment/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.six_assignment.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
